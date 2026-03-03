939d66440d725a2e9e5867e456172ea60087d76c
Saba_Zakeri_Far <szakerifar@my.bcit.ca>
2026-03-03 10:30:18 -0800

    update

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,5 @@
 // src/firebaseAPIConfig.js
+
 // -------------------------------------------------------------
 // Part of the COMP1800 Project 1 Course (BCIT).
 // Starter code provided for students to use and adapt.
@@ -11,6 +12,8 @@
 // Import Firebase SDK modules (using Firebase v9 modular syntax)
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 // ---------------------------------------------------------
 // Read Firebase configuration from Vite environment variables.
@@ -42,3 +45,4 @@ const app = initializeApp(firebaseConfig);
 // or signout operations (that's why we export it).
 // ---------------------------------------------------------
 export const auth = getAuth(app);
+export const db = getFirestore(app); 
